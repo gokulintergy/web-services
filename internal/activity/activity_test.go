b49c719e2017c840ddbcf0d442b7381441e284e4
Mike Donnici <michael@mesa.net.au>
2018-12-12 15:31:03 +1100

    refactor activity_test.go

--- internal/activity/activity_test.go
+++ internal/activity/activity_test.go
@@ -5,19 +5,17 @@ import (
 	"testing"
 
 	"github.com/cardiacsociety/web-services/internal/activity"
+	"github.com/cardiacsociety/web-services/internal/platform/datastore"
 	"github.com/cardiacsociety/web-services/testdata"
 )
 
-var db = testdata.NewDataStore()
-var helper = testdata.NewHelper()
+var store datastore.Datastore
 
-func TestActivity(t *testing.T) {
+func TestAll(t *testing.T) {
 
-	err := db.SetupMySQL()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.TearDownMySQL()
+	var teardown func()
+	store, teardown = setup()
+	defer teardown()
 
 	t.Run("activity", func(t *testing.T) {
 		t.Run("testPingDatabase", testPingDatabase)
@@ -28,76 +26,100 @@ func TestActivity(t *testing.T) {
 	})
 }
 
-func testPingDatabase(t *testing.T) {
-	err := db.Store.MySQL.Session.Ping()
+func setup() (datastore.Datastore, func()) {
+	var db = testdata.NewDataStore()
+	err := db.SetupMySQL()
 	if err != nil {
-		t.Fatal("Could not ping database")
+		log.Fatalf("db.SetupMySQL() err = %s", err)
+	}
+	return db.Store, func() {
+		err := db.TearDownMySQL()
+		if err != nil {
+			log.Fatalf("db.TearDownMySQL() err = %s", err)
+		}
+	}
+}
+
+func testPingDatabase(t *testing.T) {
+	err := store.MySQL.Session.Ping()
+	if err != nil {
+		t.Fatalf("Ping() err = %s", err)
 	}
 }
 
 func testActivityCount(t *testing.T) {
-	xa, err := activity.All(db.Store)
+	xa, err := activity.All(store)
 	if err != nil {
-		t.Fatalf("Database error: %s", err)
+		t.Fatalf("activity.All() err = %s", err)
 	}
-	helper.Result(t, 5, len(xa))
+	got := len(xa)
+	want := 5
+	if got != want {
+		t.Errorf("activity.All() = %d, want %d", got, want)
+	}
 }
 
 func testActivityTypesCount(t *testing.T) {
-
 	cases := []struct {
-		id    int
-		count int
+		arg  int
+		want int
 	}{
 		{1, 0},
 		{3, 0},
 		{20, 9},
 		{24, 5},
 	}
-
 	for _, c := range cases {
-		xa, err := activity.Types(db.Store, c.id)
+		xa, err := activity.Types(store, c.arg)
 		if err != nil {
-			t.Fatalf("Database error: %s", err)
+			t.Fatalf("activity.Types() err = %s", err)
 		}
-		helper.Result(t, c.count, len(xa))
+		got := len(xa)
+		if got != c.want {
+			t.Errorf("activity.Types() count = %d, want %d", got, c.want)
+		}
 	}
 }
 
 func testActivityByID(t *testing.T) {
 	cases := []struct {
-		id   int
-		name string
+		arg  int
+		want string
 	}{
 		{4, "Presentation"},
 		{23, "Group Learning"},
 	}
-
 	for _, c := range cases {
-		a, err := activity.ByID(db.Store, c.id)
+		a, err := activity.ByID(store, c.arg)
 		if err != nil {
-			t.Fatalf("Database error: %s", err)
+			t.Fatalf("activity.ByID() err = %s", err)
 		}
-		helper.Result(t, c.name, a.Name)
+		got := a.Name
+		if got != c.want {
+			t.Errorf("activity.ByID() Activity.Name = %q, want %q", got, c.want)
+		}
 	}
 }
 
+// fetch activity by type id, echeck the correct activity id was returned
 func testActivityByTypeID(t *testing.T) {
 	cases := []struct {
-		typeID     int
-		activityID int
+		arg  int
+		want int
 	}{
 		{2, 20},
 		{13, 21},
 		{28, 23},
 		{36, 24},
 	}
-
 	for _, c := range cases {
-		a, err := activity.ByTypeID(db.Store, c.typeID)
+		a, err := activity.ByTypeID(store, c.arg)
 		if err != nil {
-			t.Fatalf("Database error: %s", err)
+			t.Fatalf("activity.ByTypeID() err = %s", err)
 		}
-		helper.Result(t, c.activityID, a.ID)
+		got := a.ID
+		if got != c.want {
+			t.Errorf("activity.ByTypeID() Activity.ID = %d, want %d", got, c.want)
+		}
 	}
 }
