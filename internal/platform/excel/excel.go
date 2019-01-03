69c764cc7fe7323675807086bd08cf001c5c4f50
Mike Donnici <michael@mesa.net.au>
2019-01-03 17:37:30 +1100

    use excelize pkg for payment and member reports

--- internal/platform/excel/excel.go
+++ internal/platform/excel/excel.go
@@ -4,6 +4,7 @@ package excel
 import (
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 
@@ -17,21 +18,17 @@ import (
 )
 
 // MemberReport returns an excel member report File
-func MemberReport(members []member.Member) (*xlsx.File, error) {
+func MemberReport(members []member.Member) (*excelize.File, error) {
 
-	var file *xlsx.File
-	var sheet *xlsx.Sheet
-	var row *xlsx.Row
-	var cell *xlsx.Cell
-	var err error
+	var rowNum int
+	file := excelize.NewFile()
 
-	file = xlsx.NewFile()
-	sheet, err = file.AddSheet("Sheet1")
-	if err != nil {
-		return nil, fmt.Errorf("file.AddSheet() err = %s", err)
-	}
+	// heading row
+	rowNum++
+	headingStyle, _ := file.NewStyle(`{"font": {"bold": true}}`)
+	file.SetCellStyle("Sheet1", "A1", "ZZ1", headingStyle)
 
-	columns := []string{
+	xt := []string{
 		"Member ID",
 		"Prefix",
 		"First Name",
@@ -71,81 +68,86 @@ func MemberReport(members []member.Member) (*xlsx.File, error) {
 		"Second Speciality",
 		"Third Speciality",
 	}
-
-	// Column headers
-	row = sheet.AddRow()
-	for _, c := range columns {
-		cell = row.AddCell()
-		cell.Value = c
+	xc := columnKeys(len(xt))
+	for i := range xt {
+		cell := xc[i] + strconv.Itoa(rowNum) // "A1", "A2" etc
+		value := xt[i]
+		file.SetCellValue("Sheet1", cell, value)
 	}
 
-	// member rows
+	// data rows
 	for _, m := range members {
-		row = sheet.AddRow()
-		row.AddCell().Value = strconv.Itoa(m.ID)
-		row.AddCell().Value = m.Title
-		row.AddCell().Value = m.FirstName
-		row.AddCell().Value = strings.Join(m.MiddleNames, " ")
-		row.AddCell().Value = m.LastName
-		row.AddCell().Value = m.PostNominal
-		row.AddCell().Value = m.Gender
-		row.AddCell().Value = m.DateOfBirth
-		row.AddCell().Value = m.Contact.EmailPrimary
-		row.AddCell().Value = m.Contact.EmailSecondary
-		row.AddCell().Value = m.Contact.Mobile
-		row.AddCell().Value = m.DateOfEntry
+		rowNum++
+		row := make(map[string]interface{}, len(xc))
+		keys := rowKeys(xc, rowNum)
+		row[keys[0]] = strconv.Itoa(m.ID)
+		row[keys[1]] = m.Title
+		row[keys[2]] = m.FirstName
+		row[keys[3]] = strings.Join(m.MiddleNames, " ")
+		row[keys[4]] = m.LastName
+		row[keys[5]] = m.PostNominal
+		row[keys[6]] = m.Gender
+		row[keys[7]] = m.DateOfBirth
+		row[keys[8]] = m.Contact.EmailPrimary
+		row[keys[9]] = m.Contact.EmailSecondary
+		row[keys[10]] = m.Contact.Mobile
+		row[keys[11]] = m.DateOfEntry
 
 		if len(m.Memberships) > 0 {
-			row.AddCell().Value = m.Memberships[0].Title
-			row.AddCell().Value = m.Memberships[0].Status
+			row[keys[12]] = m.Memberships[0].Title
+			row[keys[13]] = m.Memberships[0].Status
 		}
 
-		row.AddCell().Value = m.Country
+		row[keys[14]] = m.Country
 
 		if len(m.Tags) > 0 {
-			row.AddCell().Value = strings.Join(m.Tags, ", ")
+			row[keys[15]] = strings.Join(m.Tags, ", ")
 		}
 
-		row.AddCell().Value = m.JournalNumber
-		row.AddCell().Value = m.BpayNumber
+		row[keys[16]] = m.JournalNumber
+		row[keys[17]] = m.BpayNumber
 
 		// ContactLocationByType returns an empty struct and an error if not found
 		// so can ignore error and write an empty cell
 		mailLocation, _ := m.ContactLocationByDesc("mail")
-		row.AddCell().Value = strings.Join(mailLocation.Address, " ")
-		row.AddCell().Value = mailLocation.City
-		row.AddCell().Value = mailLocation.State
-		row.AddCell().Value = mailLocation.Postcode
-		row.AddCell().Value = mailLocation.Country
+		row[keys[18]] = strings.Join(mailLocation.Address, " ")
+		row[keys[19]] = mailLocation.City
+		row[keys[20]] = mailLocation.State
+		row[keys[21]] = mailLocation.Postcode
+		row[keys[22]] = mailLocation.Country
 
 		directoryLocation, _ := m.ContactLocationByDesc("directory")
-		row.AddCell().Value = strings.Join(directoryLocation.Address, " ")
-		row.AddCell().Value = directoryLocation.City
-		row.AddCell().Value = directoryLocation.State
-		row.AddCell().Value = directoryLocation.Postcode
-		row.AddCell().Value = directoryLocation.Country
-		row.AddCell().Value = directoryLocation.Phone
-		row.AddCell().Value = directoryLocation.Fax
-		row.AddCell().Value = directoryLocation.Email
-		row.AddCell().Value = directoryLocation.URL
+		row[keys[23]] = strings.Join(directoryLocation.Address, " ")
+		row[keys[24]] = directoryLocation.City
+		row[keys[25]] = directoryLocation.State
+		row[keys[26]] = directoryLocation.Postcode
+		row[keys[27]] = directoryLocation.Country
+		row[keys[28]] = directoryLocation.Phone
+		row[keys[29]] = directoryLocation.Fax
+		row[keys[30]] = directoryLocation.Email
+		row[keys[31]] = directoryLocation.URL
 
 		p1, _ := m.PositionByName("First Council Affiliation")
-		row.AddCell().Value = p1.OrgName
+		row[keys[32]] = p1.OrgName
 		p2, _ := m.PositionByName("Second Council Affiliation")
-		row.AddCell().Value = p2.OrgName
+		row[keys[33]] = p2.OrgName
 		p3, _ := m.PositionByName("Third Council Affiliation")
-		row.AddCell().Value = p3.OrgName
+		row[keys[34]] = p3.OrgName
 
 		// There can be many specialities, but generally up to 3 for the report
 		// they *should* be returned in order of preference
 		if len(m.Specialities) > 0 {
-			row.AddCell().Value = m.Specialities[0].Name
+			row[keys[35]] = m.Specialities[0].Name
 		}
 		if len(m.Specialities) > 1 {
-			row.AddCell().Value = m.Specialities[1].Name
+			row[keys[36]] = m.Specialities[1].Name
 		}
 		if len(m.Specialities) > 2 {
-			row.AddCell().Value = m.Specialities[2].Name
+			row[keys[37]] = m.Specialities[2].Name
+		}
+
+		for i, t := range row {
+			file.SetCellValue("Sheet1", i, t)
 		}
 	}
 
@@ -248,68 +250,110 @@ func ApplicationReport(ds datastore.Datastore, applications []application.Applic
 func PaymentReport(ds datastore.Datastore, payments []payment.Payment) (*excelize.File, error) {
 
 	var rowNum int
-
-	// var file *xlsx.File
-	// var sheet *xlsx.Sheet
-	// var row *xlsx.Row
-	// var cell *xlsx.Cell
-	// var err error
-
 	file := excelize.NewFile()
-	//sheet := file.NewSheet("Sheet1")
-	// if err != nil {
-	// 	return nil, fmt.Errorf("file.AddSheet() err = %s", err)
-	// }
+	file.SetColWidth("Sheet1", "B", "B", 16)
+	file.SetColWidth("Sheet1", "C", "C", 20)
+	file.SetColWidth("Sheet1", "D", "D", 16)
 
-	columns := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"}
-
+	// heading row
 	rowNum++
-	titles := map[string]string{
-		"A1": "Payment ID",
-		"B1": "Payment Date",
-		"C1": "Member ID",
-		"D1": "Member",
-		"E1": "Payment Type",
-		"F1": "Amount",
-		"G1": "Comment",
-		"H1": "Field1",
-		"I1": "Field2",
-		"J1": "Field3",
-		"K1": "Field4",
-		"L1": "Invoice IDs",
+	headingStyle, _ := file.NewStyle(`{"font": {"bold": true}}`)
+	file.SetCellStyle("Sheet1", "A1", "Z1", headingStyle)
+
+	xt := []string{
+		"Payment ID",
+		"Payment Date",
+		"Member",
+		"Payment Type",
+		"Amount",
+		"Invoice",
+		"Comment",
 	}
-	for i, t := range titles {
-		file.SetCellValue("Sheet1", i, t)
+	xc := columnKeys(len(xt))
+	for i := range xt {
+		cell := xc[i] + strconv.Itoa(rowNum) // "A1", "A2" etc
+		value := xt[i]
+		file.SetCellValue("Sheet1", cell, value)
 	}
 
+	// data rows
+	var total float64
 	for _, p := range payments {
 		rowNum++
-		keys := rowKeys(columns, rowNum)
-		row := make(map[string]interface{}, len(columns))
+		keys := rowKeys(xc, rowNum)
+		row := make(map[string]interface{}, len(xc))
 		row[keys[0]] = p.ID
-		row[keys[1]] = p.Date.Format("2006-01-02")
-		row[keys[2]] = p.MemberID
-		row[keys[3]] = p.Member
-		row[keys[4]] = p.Type
-		row[keys[5]] = p.Amount
-		row[keys[6]] = p.Comment
-		row[keys[7]] = p.DataField1
-		row[keys[8]] = p.DataField2
-		row[keys[9]] = p.DataField3
-		row[keys[10]] = p.DataField4
+
+		dateStyle, err := file.NewStyle(`{"custom_number_format": "dd mmm yyyy"}`)
+		if err != nil {
+			log.Printf("NewStyle() err = %s\n", err)
+		}
+		file.SetCellStyle("Sheet1", keys[1], keys[1], dateStyle)
+		row[keys[1]] = p.Date
+
+		row[keys[2]] = p.Member + " [" + strconv.Itoa(p.MemberID) + "]"
+		row[keys[3]] = p.Type
+
+		currencyStyle, err := file.NewStyle(`{"number_format": 8}`)
+		if err != nil {
+			log.Printf("NewStyle() err = %s\n", err)
+		}
+		file.SetCellStyle("Sheet1", keys[4], keys[4], currencyStyle)
+		row[keys[4]] = p.Amount
+		total += p.Amount
 
 		var invoiceAllocations []string
 		for _, i := range p.Allocations {
 			invoiceAllocations = append(invoiceAllocations, strconv.Itoa(i.InvoiceID))
 		}
-		row[keys[11]] = strings.Join(invoiceAllocations, ", ")
+		row[keys[5]] = strings.Join(invoiceAllocations, ", ")
+
+		row[keys[6]] = p.Comment
 
 		for i, t := range row {
 			file.SetCellValue("Sheet1", i, t)
 		}
 	}
 
+	// total
+	rowNum++
+	style, err := file.NewStyle(`{"font":{"bold":true}}`)
+	if err != nil {
+		log.Printf("NewStyle() err = %s\n", err)
+	}
+	cellLabel := "D" + strconv.Itoa(rowNum)
+	file.SetCellStyle("Sheet1", cellLabel, cellLabel, style)
+	file.SetCellValue("Sheet1", cellLabel, "Total")
+	cellValue := "E" + strconv.Itoa(rowNum)
+	file.SetCellStyle("Sheet1", cellValue, cellValue, style)
+	file.SetCellValue("Sheet1", cellValue, total)
+
 	return file, nil
+}
+
+// columnkeys generates the specified number of column references - eg "A", "B" ... "Z", "AA", "AB" etc.
+func columnKeys(numCols int) []string {
+
+	result := []string{}
+	xa := []string{
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	}
+
+	for i := 0; i < numCols; i++ {
+
+		var colName string
+		var colPrefix string
+
+		set := int(math.Floor(float64(i) / float64(26)))
+		if set > 0 {
+			colPrefix = xa[set-1]
+		}
+		colName = colPrefix + xa[i-(set*26)]
+		result = append(result, colName)
+	}
+
+	return result
 }
 
 // rowKeys returns a []string containing the cell references for a row, eg "A10", "B10", "C10" etc
