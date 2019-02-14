33adcc2fc282c79ee3f86bd5bbe04be8343746b4
Mike Donnici <michael@mesa.net.au>
2019-02-14 11:07:30 +1100

    order organisations by name

--- internal/organisation/queries.go
+++ internal/organisation/queries.go
@@ -12,10 +12,10 @@ FROM
 WHERE 
 	active = 1`
 
-const querySelectParentOrganisations = querySelectActiveOrganisation + ` AND parent_organisation_id IS NULL`
+const querySelectParentOrganisations = querySelectActiveOrganisation + ` AND parent_organisation_id IS NULL ORDER BY name`
 
-const querySelectOrganisationByID = querySelectActiveOrganisation + ` AND id = ?`
+const querySelectOrganisationByID = querySelectActiveOrganisation + ` AND id = ? ORDER BY name`
 
-const querySelectOrganisationByParentID = querySelectActiveOrganisation + ` AND parent_organisation_id = ?`
+const querySelectOrganisationByParentID = querySelectActiveOrganisation + ` AND parent_organisation_id = ? ORDER BY name`
 
-const querySelectOrganisationByTypeID = querySelectActiveOrganisation + ` AND organisation_type_id = ?`
+const querySelectOrganisationByTypeID = querySelectActiveOrganisation + ` AND organisation_type_id = ? ORDER BY name`
