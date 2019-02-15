56044d1c3aef4b84ad420e0cffeaf5a632389957
Mike Donnici <michael@mesa.net.au>
2019-02-15 16:29:28 +1100

    progress #57 - errors and tests

--- internal/issue/issue.go
+++ internal/issue/issue.go
@@ -2,14 +2,21 @@ package issue
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/cardiacsociety/web-services/internal/note"
 	"github.com/cardiacsociety/web-services/internal/platform/datastore"
 )
 
+// Error messages
 const (
-	ErrorIDNotNil = "cannot insert an issue row because ID already has a value"
-	ErrorNoTypeID      = "cannot insert an issue row because Type.ID field is not set"
+	ErrorIDNotNil              = "cannot insert an issue row because ID already has a value"
+	ErrorNoTypeID              = "cannot insert an issue row because Type.ID field is not set"
+	ErrorNoDescription         = "cannot insert an issue row because Description is empty"
+	ErrorAssociationNoMemberID = "cannot associate issue with another entity unless a member id is specified"
+	ErrorAssociation           = "association entity not specified"
+	ErrorAssociationID         = "association entity ID not specified"
+	ErrorAssociationEntity     = "association entity unknown"
 )
 
 // Issue represents a workflow issue
@@ -21,6 +28,16 @@ type Issue struct {
 	Description string
 	Action      string
 	Notes       []note.Note
+
+	// The following fields represent data associated with an issue. In the relational database this
+	// is how Issues are linked to members and invoices. The association is optionsal and open
+	// so as to allow Issue to be raised without any association (gloabl issues) or specifically
+	//related to a member or invoice record. As such, any connections must be determined programatically.
+	// At this stage issues can only be associated with "application" or "invoice" records
+
+	MemberID      int    // if set, this issue will be associated with this member
+	Association   string // either "application" or "invoice"
+	AssociationID int    // the id of the associated application or invoice record
 }
 
 // Type represents the sub-category of the issue, ie Category -> Type
@@ -40,15 +57,51 @@ type Category struct {
 
 // InsertRow creates a new issue row with fields from Issue
 func (i *Issue) InsertRow(ds datastore.Datastore) error {
-
-	if i.ID > 0 {
+	switch {
+	case i.ID > 0:
 		return errors.New(ErrorIDNotNil)
-	}
-	if i.Type.ID == 0 {
+	case i.Type.ID == 0:
 		return errors.New(ErrorNoTypeID)
+	case i.Description == "":
+		return errors.New(ErrorNoDescription)
+	}
+	q := fmt.Sprintf(queries["insert-issue"], i.Type.ID, i.Description, i.Action)
+	_, err := ds.MySQL.Session.Exec(q)
+	if err != nil {
+		return err
 	}
 
+	// Associate other data if fields are set, but ensure data looks ok
+	err = i.checkAssociatioData()
+	if err != nil {
+		return err
+	}
 
-	//q := fmt.Sprintf(queries["insert-issue"], i.Type.ID, i.Description, i.Action)
+	return nil
+}
+
+// checkAssociatioData verifies fields required to associate an issue with other data
+func (i *Issue) checkAssociatioData() error {
+	// no association
+	if i.MemberID == 0 && i.Association == "" && i.AssociationID == 0 {
+		return nil
+	}
+	// associate with member only
+	if i.MemberID > 0 && i.Association == "" && i.AssociationID == 0 {
+		return nil
+	}
+	// associate with entity
+	if i.Association != "" || i.AssociationID > 0 {
+		switch {
+		case i.MemberID == 0:
+			return errors.New(ErrorAssociationNoMemberID)
+		case i.Association == "":
+			return errors.New(ErrorAssociation)
+		case i.AssociationID == 0:
+			return errors.New(ErrorAssociationID)
+		case i.Association != "application" && i.Association != "invoice":
+			return errors.New(ErrorAssociationEntity)
+		}
+	}
 	return nil
 }
--- internal/issue/issue_test.go
+++ internal/issue/issue_test.go
@@ -13,14 +13,20 @@ var ds datastore.Datastore
 
 func TestIssue(t *testing.T) {
 
-	var teardown func()
-	ds, teardown = setup()
-	defer teardown()
+	// var teardown func()
+	ds, _ = setup()
+	// defer teardown()
 
 	t.Run("issue", func(t *testing.T) {
 		t.Run("testPingDatabase", testPingDatabase)
 		t.Run("testInsertRowErrorIDNotNil", testInsertRowErrorIDNotNil)
 		t.Run("testInsertRowErrorNoTypeID", testInsertRowErrorNoTypeID)
+		t.Run("testInsertRowErrorNoDescription", testInsertRowErrorNoDescription)
+		t.Run("testInsertRowErrorAssociationNoMemberID", testInsertRowErrorAssociationNoMemberID)
+		t.Run("testInsertRowErrorAssociation", testInsertRowErrorAssociation)
+		t.Run("testInsertRowErrorAssociationID", testInsertRowErrorAssociationID)
+		t.Run("testInsertRowErrorAssociationEntity", testInsertRowErrorAssociationEntity)
+		t.Run("testInsertRow", testInsertRow)
 	})
 }
 
@@ -56,7 +62,7 @@ func testInsertRowErrorIDNotNil(t *testing.T) {
 	err := i.InsertRow(ds)
 	want := issue.ErrorIDNotNil
 	if err == nil {
-		t.Errorf("Issue.InsertRow() err = nil, want %s", want)
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
 	}
 }
 
@@ -69,6 +75,96 @@ func testInsertRowErrorNoTypeID(t *testing.T) {
 	err := i.InsertRow(ds)
 	want := issue.ErrorNoTypeID
 	if err == nil {
-		t.Errorf("Issue.InsertRow() err = nil, want %s", want)
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+// test an attempt to insert an issue row with Issue.Description not set
+func testInsertRowErrorNoDescription(t *testing.T) {
+	i := issue.Issue{
+		Type:   issue.Type{ID: 2},
+		Action: "This is what must be done",
+	}
+	err := i.InsertRow(ds)
+	want := issue.ErrorNoDescription
+	if err == nil {
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+func testInsertRowErrorAssociationNoMemberID(t *testing.T) {
+	i := issue.Issue{
+		Type:          issue.Type{ID: 2},
+		Description:   "This is the description",
+		Action:        "This is what must be done",
+		MemberID:      0, // err
+		AssociationID: 345,
+		Association:   "application",
+	}
+	err := i.InsertRow(ds)
+	want := issue.ErrorAssociationNoMemberID
+	if err == nil {
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+func testInsertRowErrorAssociation(t *testing.T) {
+	i := issue.Issue{
+		Type:          issue.Type{ID: 2},
+		Description:   "This is the description",
+		Action:        "This is what must be done",
+		MemberID:      123,
+		AssociationID: 345,
+		Association:   "", // err
+	}
+	err := i.InsertRow(ds)
+	want := issue.ErrorAssociation
+	if err == nil {
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+func testInsertRowErrorAssociationID(t *testing.T) {
+	i := issue.Issue{
+		Type:          issue.Type{ID: 2},
+		Description:   "This is the description",
+		Action:        "This is what must be done",
+		MemberID:      123,
+		AssociationID: 0, // err
+		Association:   "application",
+	}
+	err := i.InsertRow(ds)
+	want := issue.ErrorAssociationID
+	if err == nil {
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+func testInsertRowErrorAssociationEntity(t *testing.T) {
+	i := issue.Issue{
+		Type:          issue.Type{ID: 2},
+		Description:   "This is the description",
+		Action:        "This is what must be done",
+		MemberID:      123,
+		AssociationID: 345,
+		Association:   "unknownentity", // err
+	}
+	err := i.InsertRow(ds)
+	want := issue.ErrorAssociationEntity
+	if err == nil {
+		t.Errorf("Issue.InsertRow() err = nil, want %q", want)
+	}
+}
+
+// test insert an error row
+func testInsertRow(t *testing.T) {
+	i := issue.Issue{
+		Type:        issue.Type{ID: 2},
+		Description: "This is the description",
+		Action:      "This is what must be done",
+	}
+	err := i.InsertRow(ds)
+	if err != nil {
+		t.Errorf("Issue.InsertRow() err = %s", err)
 	}
 }
